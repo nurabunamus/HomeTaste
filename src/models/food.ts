d97933af64032c962ef419700c3b9dd6db2abc6f
MasterOfAlune <masterofalune@gmail.com>
2023-07-07 08:19:39 +0300

    Feat: add food model and interface

---
+++ src/models/food.ts
@@ -0,0 +1,40 @@
+import { Document, Types, Schema, model } from 'mongoose';
+
+enum Categories {
+  // add your categories here
+}
+
+enum Allergies {
+  // add your allergies here
+}
+
+interface IFood extends Document {
+  user_id: Types.ObjectId;
+  name: string;
+  description: string;
+  price: number;
+  image: string;
+  categories: string;
+  allergies: string;
+}
+
+const foodSchema = new Schema<IFood>({
+  user_id: { type: Schema.Types.ObjectId, ref: 'User' },
+  name: { type: String, required: true, minlength: 3 },
+  description: { type: String, maxlength: 256},
+  price: {
+    type: Number,
+    validate: {
+      validator: function (value: number) {
+        return value > 0;
+      },
+      message: 'Price must be a positive number',
+    },
+  },
+  image: String,
+  categories: { type: String, enum: Object.values(Categories) },
+  allergies: { type: String, enum: Object.values(Allergies) },
+});
+
+
+const Food = model<IFood>('Food', foodSchema);
