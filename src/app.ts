275389cd650bcff1adc3cc6b2f3305e760589a3a
MasterOfAlune <masterofalune@gmail.com>
2023-07-09 09:38:18 +0300

    Refactor: update app.ts with errors-handling setup

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
+import errorHandler from './middlewares/error-handling';
 import connectToMongo from '../src/db/connection';
 
 const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+app.use('*', errorHandler)
 
 const port = process.env.NODE_LOCAL_PORT || 4000;
 
