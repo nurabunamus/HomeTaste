091c604c6dc5fdd72fc1ef171fa18281275bd2bc
MasterOfAlune <masterofalune@gmail.com>
2023-07-09 09:34:56 +0300

    Feat: add customError class

---
+++ src/errors/customErrors.ts
@@ -0,0 +1,14 @@
+abstract class customError extends Error {
+    abstract errorCode: number
+    abstract errorType: string
+
+    constructor(message: string) {
+        super(message)
+
+        Object.setPrototypeOf(this, customError)
+    }
+
+    abstract serializeErrors(): { message: string, property?: string }[]
+}
+
+export default customError
