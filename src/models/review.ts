39191e0bed3f78a52b441e74b25fe8ba08a07daa
MOHAMMAD ALMOHAMMAD <neotaiki123@gmail.com>
2023-07-10 19:15:06 +0300

    Fix: add Review model, Issue#4 is now done

---
+++ src/models/review.ts
@@ -0,0 +1,37 @@
+import { Schema, Model, model, Types } from 'mongoose';
+import { orderSchema, IOrder } from './order';
+
+export interface IReview {
+  rating: number;
+  comment: string;
+  userId: Types.ObjectId;
+  dishId: Types.ObjectId;
+  order: IOrder;
+}
+
+/* check the mongoose typescript documentation for more info
+ https://mongoosejs.com/docs/typescript/subdocuments.html */
+type ReviewDocumentOverrides = {
+  order: Types.Subdocument<Types.ObjectId> & IOrder;
+};
+
+type ReviewModelType = Model<IReview, {}, ReviewDocumentOverrides>;
+
+export const ReviewSchema = new Schema<IReview, ReviewModelType>({
+  rating: Number,
+  comment: String,
+  userId: { type: Schema.Types.ObjectId, required: true },
+  dishId: { type: Schema.Types.ObjectId, required: true },
+  order: { type: orderSchema, required: true },
+});
+
+// makes sure each review record has a comment and/or rating, we cant have a review without both a rating AND a comment
+ReviewSchema.pre<IReview>('save', function (next): any {
+  return this.rating && this.comment
+    ? next()
+    : 'cant save a review without a comment and/or rating';
+});
+
+const Review = model<IReview>('Review', ReviewSchema);
+
+export default Review;
