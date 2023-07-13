231138d0939e2ea24fd9654235527de401bf76a0
Fatima Ali <fofoalmali@gmail.com>
2023-07-13 14:04:21 +0300

    Feat:update google routes #6

--- src/routes/google.ts
+++ src/routes/google.ts
@@ -1,10 +1,16 @@
+/* eslint-disable prettier/prettier */
 /* eslint-disable import/newline-after-import */
 /* eslint-disable node/no-unsupported-features/es-syntax */
-import express from 'express';
+import express, { Request, Response } from 'express';
 import passport from '../config/passport';
 import saveGoogle from '../controllers/google';
+
 const router = express.Router();
 
+/**
+ * Initiates the Google authentication process.
+ * Redirects the user to the Google authentication page.
+ */
 router.get(
   '/google',
   passport.authenticate('google', {
@@ -12,6 +18,12 @@ router.get(
   })
 );
 
+/**
+ * Callback route for Google authentication.
+ * Handles the authentication callback from Google after successful authentication.
+ * If authentication fails, redirects the user back to the Google authentication page.
+ * If authentication succeeds, saves the user data and generates a JSON Web Token (JWT).
+ */
 router.get(
   '/google/callback',
   passport.authenticate('google', {
@@ -20,5 +32,14 @@ router.get(
   }),
   saveGoogle
 );
+/**
+ * Route for user logout.
+ * Clears the authentication token cookie from the response and sends a success message.
+ */
+router.get('/logout', (req: Request, res: Response) => {
+  res.clearCookie('auth_token');
+  res.send('Cookie deleted successfully');
+  res.end();
+});
 
 export default router;
