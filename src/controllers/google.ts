33a382361abe994a0bc950b08b18d27a309e4c70
Fatima Ali <fofoalmali@gmail.com>
2023-07-14 21:17:54 +0300

    Feat: update save google function

--- src/controllers/google.ts
+++ src/controllers/google.ts
@@ -1,9 +1,9 @@
 /* eslint-disable no-underscore-dangle */
 /* eslint-disable node/no-unsupported-features/es-syntax */
 import { Request, Response } from 'express';
-import jwt, { Secret } from 'jsonwebtoken';
 import User from '../models/user';
 import { IUser } from '../types/interfaces';
+import { setTokenCookie } from '../utils/auth';
 
 /**
  * Saves user data received from Google authentication.
@@ -20,35 +20,34 @@ type JsonType = {
 };
 
 async function saveGoogle(req: Request, res: Response) {
-  const userReq = req.user as IUser & { _json: JsonType };
-  const googleId = `google-${userReq._json.sub}`;
-  const user = await User.findOne({ provider_id: googleId });
-  if (!user) {
-    const newUser = await User.create({
-      first_name: userReq._json.given_name,
-      last_name: userReq._json.family_name,
-      email: userReq._json.email,
-      provider_id: googleId,
-    });
-    await newUser.save();
+  try {
+    const userReq = req.user as IUser & { _json: JsonType };
+    const googleId = `google-${userReq._json.sub}`;
+    const user = await User.findOne({ email: userReq._json.email });
+    if (!user) {
+      const newUser = await User.create({
+        first_name: userReq._json.given_name,
+        last_name: userReq._json.family_name,
+        email: userReq._json.email,
+        provider_id: googleId,
+      });
+      await newUser.save();
+      setTokenCookie(newUser._id, newUser.role, newUser.fullName, res);
 
-    const secretKey = process.env.SECRET_KEY as Secret;
-    const payload = {
-      id: newUser._id,
-      fullName: `${newUser.first_name} ${newUser.last_name}`,
-      email: newUser.email,
-    };
-    const token = jwt.sign(payload, secretKey, { expiresIn: '14 days' });
-    res.cookie('auth_token', token, {
-      httpOnly: true,
-      signed: true,
-      expires: new Date(Date.now() + 14 * 24 * 60 * 60 * 1000),
-      secure: false,
-    });
-    res.status(200).json({
-      message: 'User successfully signed in',
-      user: payload,
-    });
+      res.status(200).json({
+        message: 'User successfully signed in',
+        user: {
+          id: newUser._id,
+          fullName: newUser.fullName,
+          email: newUser.email,
+        },
+      });
+    } else {
+      res.status(400).send({ error: 'User already exists' });
+      return;
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'Internal server error' });
   }
 }
 
