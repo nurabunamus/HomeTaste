36be090b6ebe405858ff1e507257c83d77a0885d
MOHAMMAD ALMOHAMMAD <neotaiki123@gmail.com>
2023-07-16 23:42:06 +0300

    Feat: add passport facebook authentication strategy

--- src/middlewares/authentication.ts
+++ src/middlewares/authentication.ts
@@ -1,16 +1,27 @@
 import { Request } from 'express';
 import passport from 'passport';
 import passportFacebook, { Profile } from 'passport-facebook';
+import passportGoogle from 'passport-google-oauth20';
 import passportJWT from 'passport-jwt';
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
 import { HydratedDocument } from 'mongoose';
 import User from '../models/user';
 import { IUser } from '../types/interfaces';
+import createJWTToken from '../utils/auth';
+
+const {
+  GOOGLE_CLIENT_ID,
+  GOOGLE_CLIENT_SECRET,
+  BASE_URL,
+  CLIENT_ID_FB,
+  CLIENT_SECRET_FB,
+  JWT_SECRET_KEY,
+} = process.env;
 
 dotenv.config();
 
-/* This File Contains The Implementations For All tThe Strategies, Current Passport Strategies That Are Used In This Project Are:
+/* This File Contains The Implementations For All The Strategies, Current Passport Strategies That Are Used In This Project Are:
     1- Facebook, To Provide Facebook Login Service
     2- Google, To Provide Google Login Service
     3- JWT, To Check The JWT Token
@@ -21,9 +32,9 @@ const FacebookStrategy = passportFacebook.Strategy;
 passport.use(
   new FacebookStrategy(
     {
-      clientID: process.env.CLIENT_ID_FB!,
-      clientSecret: process.env.CLIENT_SECRET_FB!,
-      callbackURL: 'http://localhost:3000/api/auth/facebook/callback',
+      clientID: CLIENT_ID_FB!,
+      clientSecret: CLIENT_SECRET_FB!,
+      callbackURL: `${BASE_URL}/api/auth/facebook/callback`,
       profileFields: ['id', 'displayName', 'photos', 'email', 'name'],
       passReqToCallback: true,
     },
@@ -38,21 +49,28 @@ passport.use(
         const existingUser = await User.findOne({ provider_id: profile.id });
 
         if (existingUser) {
-          return done(undefined, existingUser);
+          const newToken = createJWTToken(
+            existingUser._id,
+            existingUser.fullName,
+            existingUser.email
+          );
+          return done(null, newToken);
         }
+
         const existingEmailUser = await User.findOne({
           email: profile._json.email,
         });
 
         if (existingEmailUser) {
-          throw new Error(
+          return done(
+            null,
+            false,
             'There is already another user with this email address, please use another email'
           );
         }
         const createdUser: HydratedDocument<IUser> = await User.create({
           email: profile._json.email,
           provider_id: profile.id,
-          role: 'customer',
           first_name: profile.name!.givenName,
           last_name: profile.name!.familyName,
 
@@ -61,28 +79,15 @@ passport.use(
 
         if (!createdUser) {
           throw new Error(
-            'Cant create an account due to a database issue, please try again later'
+            'Cant make a new account due to a database issue, please try again later'
           );
         }
 
-        const expiresIn = 14 * 24 * 60 * 60; // 14 days in seconds
-        const now = Math.floor(Date.now() / 1000); // Convert milliseconds to seconds
-        const expiresAt = now + expiresIn;
-
-        const user = {
-          email: createdUser.email,
-          providerId: `facebook-${createdUser.provider_id}`,
-          avatar: createdUser.profile_image,
-          role: createdUser.role,
-        };
-
-        const token = jwt.sign(
-          {
-            ...user,
-            exp: expiresAt,
-            iat: now,
-          },
-          process.env.JWT_SECRET_KEY!
+        const token = createJWTToken(
+          createdUser._id,
+          createdUser.role,
+          createdUser.fullName,
+          createdUser.email
         );
         return done(null, token);
       } catch (err) {
@@ -93,26 +98,23 @@ passport.use(
 );
 
 // Google Strategy
-
-// JWT Strategy
-const JWTStrategy = passportJWT.Strategy;
-const jwtOptions = {
-  jwtFromRequest: (req: any) => req.cookies.token,
-  secretOrKey: process.env.JWT_SECRET_KEY!,
-};
+const GoogleStrategy = passportGoogle.Strategy;
 
 passport.use(
-  new JWTStrategy(jwtOptions, async (token: any, done: any) => {
-    const user = await User.findOne({ email: token.email });
-    try {
-      if (user) {
-        return done(undefined, user, token);
+  new GoogleStrategy(
+    {
+      clientID: GOOGLE_CLIENT_ID as string,
+      clientSecret: GOOGLE_CLIENT_SECRET as string,
+      callbackURL: `${BASE_URL}/api/auth/google/callback`,
+    },
+    async (accessToken, refreshToken, profile, cb) => {
+      try {
+        cb(null, profile as Profile);
+      } catch (error) {
+        cb(null, error as Error);
       }
-      return done(undefined, false);
-    } catch (err) {
-      return done(err, false);
     }
-  })
+  )
 );
 
 export default passport;
