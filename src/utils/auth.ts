783405a61a334f235c86a07e6b8cc7475f5cb771
MOHAMMAD ALMOHAMMAD <neotaiki123@gmail.com>
2023-07-16 23:48:22 +0300

    Refactor: refactor setTokenCookie to make it work with passport middleware and rename the function to have a clearer name

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,40 +1,25 @@
-/* eslint-disable import/prefer-default-export */
-/* eslint-disable node/no-unsupported-features/es-syntax */
-// eslint-disable-next-line node/no-unsupported-features/es-syntax
 import jwt, { Secret } from 'jsonwebtoken';
 import { Response } from 'express';
+import dotenv from 'dotenv';
 
-const setTokenCookie = (
+dotenv.config();
+
+const createJWTToken: any = (
   userId: string,
-  role: string,
   fullName: string,
-  res: Response,
-  clearExisting = false
-): void => {
-  if (clearExisting) {
-    res.clearCookie('auth_token');
-  }
-
+  email: string
+): string => {
   const payload = {
     _id: userId,
-    // eslint-disable-next-line object-shorthand
     fullName: fullName,
-    // eslint-disable-next-line object-shorthand
-    role: role,
+    email: email,
   };
 
-  const token = jwt.sign(payload, process.env.SECRET_KEY as Secret, {
+  const token = jwt.sign(payload, process.env.JWT_SECRET_KEY!, {
     expiresIn: '14 days',
   });
-  res.cookie('auth_token', token, {
-    httpOnly: true,
-    signed: true,
-    expires: new Date(Date.now() + 14 * 24 * 60 * 60 * 1000),
-    secure: false,
-    // We should add them when we are going to deploy our app
-    // secure: process.env.DEPLOYED === 'yes',
-    // sameSite: 'none',
-  });
+
+  return token;
 };
 
-export { setTokenCookie };
+export default createJWTToken;
