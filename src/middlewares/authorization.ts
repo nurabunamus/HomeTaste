deb6cbf37772a11b59e025e30465cde89995297f
MasterOfAlune <masterofalune@gmail.com>
2023-07-17 08:15:42 +0300

    Feat: add authorization, function

---
+++ src/middlewares/authorization.ts
@@ -0,0 +1,25 @@
+// eslint-disable-next-line node/no-unsupported-features/es-syntax
+import { Request, Response, NextFunction } from 'express';
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace Express {
+    interface Request {
+      user_cookie?: { role: string };
+    }
+  }
+}
+
+// eslint-disable-next-line arrow-body-style
+const checkRole = (role: string) => {
+  // eslint-disable-next-line consistent-return
+  return (req: Request, res: Response, next: NextFunction) => {
+    if (req?.user_cookie?.role !== role) {
+      return res.status(403).json({ errorMessage: 'Forbidden' });
+    }
+    next();
+  };
+};
+
+// eslint-disable-next-line node/no-unsupported-features/es-syntax
+export default checkRole;
