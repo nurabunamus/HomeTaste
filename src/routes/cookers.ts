a0e1bd4388b3478dbdc18a6915c6adbee4533b74
MasterOfAlune <masterofalune@gmail.com>
2023-07-24 16:13:25 +0300

    Feat: add setup for updateDish

--- src/routes/cookers.ts
+++ src/routes/cookers.ts
@@ -1,11 +1,12 @@
 // eslint-disable-next-line node/no-unsupported-features/es-syntax
 import express from 'express';
 // eslint-disable-next-line node/no-unsupported-features/es-syntax
-import createDish from '../controllers/cooker';
+import { createDish, updateDish } from '../controllers/cooker';
 
 const cookerRouter = express.Router();
 
 cookerRouter.post('/:cookerId/dish', createDish);
+cookerRouter.put('/:cookerId/:dishId', updateDish);
 
 // eslint-disable-next-line node/no-unsupported-features/es-syntax
 export default cookerRouter;
