5c707645c16f050f36f56a54a910af0b03aeddb9
Fatima Ali <fofoalmali@gmail.com>
2023-07-25 12:46:10 +0300

    Feat: add getUserProfile function #40

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,3 +1,4 @@
+/* eslint-disable spaced-comment */
 /* eslint-disable node/no-unsupported-features/es-syntax */
 /* eslint-disable prettier/prettier */
 /* eslint-disable object-shorthand */
@@ -8,6 +9,42 @@ import { Request, Response } from 'express';
 import User from '../models/user';
 import { IAddress, IUser } from '../types/interfaces';
 
+interface userProfile {
+  first_name: string;
+  last_name: string;
+  email: string;
+  phone: string;
+  cooker_status: string;
+  address: IAddress;
+  profile_image: string;
+}
+export const getUserProfile = async (req: Request, res: Response) => {
+  try {
+    const userReq = req.user as IUser;
+    const user = await User.findById({ _id: userReq._id });
+
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+    const userProfile: userProfile = {
+      first_name: user.first_name,
+      last_name: user.last_name,
+      email: user.email,
+      phone: user.phone,
+      cooker_status: user.cooker_status,
+      address: user.address,
+      profile_image: user.profile_image,
+    };
+
+    return res.status(200).json({
+      message: 'User retrieved successfully',
+      data: userProfile,
+    });
+  } catch (error) {
+    return res.status(500).json({ error: 'Internal server error' });
+  }
+};
+
 interface updateProfile {
   address: IAddress;
   phone: string;
@@ -16,12 +53,31 @@ interface updateProfile {
   first_name: string;
   last_name: string;
 }
-
 export const updateUserProfile = async (req: Request, res: Response) => {
   try {
     const userReq = req.user as IUser;
-    const { first_name, last_name, phone, cooker_status, address } = req.body;
-
+    const {
+      first_name,
+      last_name,
+      phone,
+      cooker_status,
+      street_name,
+      street_number,
+      city,
+      state,
+      flat_number,
+      district,
+      zip,
+    } = req.body;
+    const address: IAddress = {
+      street_name,
+      street_number,
+      state,
+      city,
+      flat_number,
+      district,
+      zip,
+    };
     // Assigns the server path of the uploaded profile image to the 'profile_image' property in the request body (if available).
     req.body.profile_image = req.file?.path;
 
