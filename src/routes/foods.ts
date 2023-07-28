31c33aa50f7997c5c7d07fc56d30fb1ac197ac7b
Ammar Almuain <116968806+MasterOfAlune@users.noreply.github.com>
2023-07-28 18:31:16 +0300

    Update foods.ts

--- src/routes/foods.ts
+++ src/routes/foods.ts
@@ -8,5 +8,6 @@ const foodRouter = express.Router();
 foodRouter.get('/:foodId', getFoodById);
 foodRouter.get('/', getFoodFilter);
 
+
 // eslint-disable-next-line node/no-unsupported-features/es-syntax
 export default foodRouter;
