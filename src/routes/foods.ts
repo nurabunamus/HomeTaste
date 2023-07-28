54681fedeea62262d69503797f0c63db477309ff
Ammar Almuain <116968806+MasterOfAlune@users.noreply.github.com>
2023-07-28 18:29:48 +0300

    Update foods.ts

--- src/routes/foods.ts
+++ src/routes/foods.ts
@@ -1,11 +1,12 @@
 // eslint-disable-next-line node/no-unsupported-features/es-syntax
 import express from 'express';
 // eslint-disable-next-line node/no-unsupported-features/es-syntax
-import getFoodById from '../controllers/food';
+import { getFoodById, getFoodFilter } from '../controllers/food';
 
 const foodRouter = express.Router();
 
 foodRouter.get('/:foodId', getFoodById);
+foodRouter.get('/', getFoodFilter);
 
 // eslint-disable-next-line node/no-unsupported-features/es-syntax
 export default foodRouter;
