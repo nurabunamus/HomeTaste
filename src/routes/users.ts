6ff80bf17f30c4f03006a0dcdd540334517968f1
Fatima Ali <fofoalmali@gmail.com>
2023-07-30 13:18:23 +0300

    Refactor: update user controller and tests, and user.yaml documentation #73

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,21 +1,15 @@
-/* eslint-disable spaced-comment */
-/* eslint-disable prettier/prettier */
-/* eslint-disable node/no-unsupported-features/es-syntax */
 import express from 'express';
-import userControllers from '../controllers/user'; // Import the controller function
-import upload from '../middlewares/multer'; // Import the Multer middleware
-import isAuthorized from '../middlewares/isAuth';
+import userControllers from '../controllers/user';
+import upload from '../middlewares/multer';
+import { isAuthenticated } from '../middlewares/isAuth';
 
 const router = express.Router();
 
-router.get('/profile', isAuthorized, userControllers.getUserProfile);
+router.get('/profile', isAuthenticated, userControllers.getUserProfile);
 
-/* PATCH route for updating user profile information, including the profile image.
- * This route is protected and requires the user to be authorized using the 'isAuthorized' middleware.
- */
 router.patch(
   '/profile/edit',
-  isAuthorized,
+  isAuthenticated,
   upload.single('profile_image'),
   userControllers.updateUserProfile
 );
