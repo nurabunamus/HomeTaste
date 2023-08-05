cb9399ea08f8b5e7e2cc06a61cca96b9d30ad431
Fatima Ali <fofoalmali@gmail.com>
2023-08-05 18:22:20 +0300

    Feat: update order controllers #93

--- src/controllers/order.ts
+++ src/controllers/order.ts
@@ -1,8 +1,9 @@
 import { Request, Response } from 'express';
 import Order, { OrderStatus } from '../models/order';
-import { IOrder, IUser } from '../types/interfaces';
+import { IUser } from '../types/interfaces';
 import User from '../models/user';
 import Cart from '../models/cart';
+import Food from '../models/food';
 
 const getOrders = async (req: Request, res: Response) => {
   try {
@@ -24,14 +25,17 @@ const getOrders = async (req: Request, res: Response) => {
     const formattedOrders = orders.map((order) => ({
       _id: order._id,
       orderDetails: order.orderDetails,
+      totalPrice: order.totalPrice,
       orderStatus: order.orderStatus,
       user: {
+        _id: user._id,
         first_name: user.first_name,
         last_name: user.last_name,
         email: user.email,
         phone: user.phone,
         address: user.address,
       },
+      cookerId: order.cookerId,
     }));
 
     return res.status(200).json({
@@ -46,36 +50,47 @@ const getOrders = async (req: Request, res: Response) => {
 const createOrder = async (req: Request, res: Response) => {
   try {
     const userReq = req.user as IUser;
-    const { cookerId } = req.body as IOrder;
-
-    // Check if the provided cookerId belongs to a user with the role "cooker"
-    const cooker = await User.findById(cookerId);
-    if (!cooker || cooker.role !== 'cooker') {
-      return res.status(403).json({ message: 'User is not a cooker' });
-    }
-
     // Find the user associated with the authenticated user ID
     const user = await User.findById(userReq._id);
     if (!user) {
       return res.status(404).json({ message: 'User not found' });
     }
-
     // Find the user's cart in the database
     const cart = await Cart.findOne({ user: user._id });
     if (!cart) {
       return res.status(404).json({ message: 'No cart found for this user' });
     }
+    // Check if the cart has at least one item
+    if (!cart.items || cart.items.length === 0) {
+      return res.status(404).json({ error: 'Cart is empty' });
+    }
+    // Get the first item in the cart
+    const firstCartItem = cart.items[0];
+
+    // Check if the first item has a dishId
+    if (!firstCartItem || !firstCartItem.dishId) {
+      return res
+        .status(404)
+        .json({ error: 'No dishId found in the first item of the cart' });
+    }
+    // Extract the dishId from the first item in the cart
+    const dishId = firstCartItem.dishId.toString();
+    const food = await Food.findById({ _id: dishId });
+    if (!food) {
+      return res.status(404).json({ error: 'Food item not found' });
+    }
+    const cookerId = food?.user_id;
 
     const newOrder = await Order.create({
       orderDetails: cart.items,
+      totalPrice: cart.totalPrice,
       user,
       cookerId,
     });
 
     // Clear the cart items after the order is successfully created
-    cart.items = [];
-    cart.totalPrice = 0;
-    await cart.save;
+    cart.set({ items: [] });
+    cart.save();
 
     return res
       .status(200)
@@ -107,6 +122,7 @@ const cancelOrder = async (req: Request, res: Response) => {
     const formattedOrder = {
       _id: order._id,
       orderDetails: order.orderDetails,
+      totalPrice: order.totalPrice,
       orderStatus: order.orderStatus,
       user: {
         first_name: order.user.first_name,
@@ -115,6 +131,7 @@ const cancelOrder = async (req: Request, res: Response) => {
         phone: order.user.phone,
         address: order.user.address,
       },
+      cookerId: order.cookerId,
     };
     return res
       .status(200)
