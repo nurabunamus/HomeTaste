86458ed784c836d31adf154c82c44d076f711867
Nur <noorin.sa.99@gmail.com>
2023-08-06 16:31:14 +0500

    Test: add test for login controller #83

--- src/controllers/__tests__/auth.test.ts
+++ src/controllers/__tests__/auth.test.ts
@@ -1,4 +1,5 @@
 import request from 'supertest';
+import bcrypt from 'bcrypt';
 import app from '../../app';
 import { connectToMongo, closeDbConnection } from '../../db/connection';
 import User from '../../models/user';
@@ -20,11 +21,30 @@ const mockUser = {
   save: jest.fn(),
 };
 
+const mockLoginUser = {
+  email: 'test99@example.com',
+  password: 'password123',
+};
+
+const user = {
+  _id: 'valid_user_id_here',
+  // Other user properties...
+};
+
 const spyUserFind1 = jest.spyOn(User, 'findOne').mockReturnValue({} as any);
+const spyUserFindLogin = jest
+  .spyOn(User, 'findOne')
+  .mockReturnValue(mockLoginUser as any);
+
+const spyBycrypt = jest
+  .spyOn(bcrypt, 'compare')
+  .mockImplementation(() => Promise.resolve(true));
 
 describe('Auth Routes', () => {
   afterEach(() => {
     spyUserFind1.mockClear();
+    spyUserFindLogin.mockClear();
+    spyBycrypt.mockClear();
   });
 
   describe('POST api/auth/register1', () => {
@@ -49,6 +69,20 @@ describe('Auth Routes', () => {
       expect(spyUserFind2).toBeCalledTimes(1);
       expect(res.status).toBe(409);
       expect(res.body.error).toBe('User already exists');
+    });
+  });
+
+  describe('POST api/auth/login', () => {
+    it('should return 200 if login is successful', async () => {
+      jest.spyOn(User, 'findOne').mockResolvedValue(user);
+
+      const res = await request(app)
+        .post('/api/auth/login')
+        .send(mockLoginUser);
+      expect(spyUserFindLogin).toBeCalledTimes(1);
+      expect(spyBycrypt).toBeCalledTimes(1);
+      expect(res.status).toBe(200);
+      expect(res.body.message).toBe('User successfully logged in');
     });
   });
 });
