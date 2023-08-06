c1bf448b28c175a56b781a8a2cea4c2cfbb31771
Nur <noorin.sa.99@gmail.com>
2023-08-06 16:02:32 +0500

    Test: add rest of tests for register1 controller #83

--- src/controllers/__tests__/auth.test.ts
+++ src/controllers/__tests__/auth.test.ts
@@ -13,26 +13,42 @@ afterAll(async () => {
 });
 
 const mockUser = {
-  email: 'test222@example.com',
+  email: 'test99@example.com',
   password: 'password123',
   fullName: 'John Doe',
   __v: 0,
   save: jest.fn(),
 };
 
-const spyUserFind = jest.spyOn(User, 'findOne').mockReturnValue({} as any);
+const spyUserFind1 = jest.spyOn(User, 'findOne').mockReturnValue({} as any);
 
 describe('Auth Routes', () => {
   afterEach(() => {
-    spyUserFind.mockClear();
+    spyUserFind1.mockClear();
   });
 
   describe('POST api/auth/register1', () => {
     it('should register a new user', async () => {
       const res = await request(app).post('/api/auth/register1').send(mockUser);
-      expect(spyUserFind).toBeCalledTimes(1);
+      expect(spyUserFind1).toBeCalledTimes(1);
       expect(res.status).toBe(201);
       expect(res.body.message).toBe('User successfully signed up');
+    });
+
+    it('should return 400 if required fields are missing', async () => {
+      const res = await request(app).post('/api/auth/register1').send({});
+      expect(res.status).toBe(400);
+      expect(res.body.error).toBe('Missing required fields');
+    });
+
+    it('should return 409 if user already exists', async () => {
+      const spyUserFind2 = jest
+        .spyOn(User, 'findOne')
+        .mockResolvedValueOnce(mockUser);
+      const res = await request(app).post('/api/auth/register1').send(mockUser);
+      expect(spyUserFind2).toBeCalledTimes(1);
+      expect(res.status).toBe(409);
+      expect(res.body.error).toBe('User already exists');
     });
   });
 });
