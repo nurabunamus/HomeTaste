fa3b721a7f625220bf370e0754ebdef894c0b268
Nur <noorin.sa.99@gmail.com>
2023-08-08 21:35:55 +0500

    Refactor: Fix bugs #100

--- src/middlewares/error-handling.ts
+++ src/middlewares/error-handling.ts
@@ -1,9 +1,15 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import customError from '../errors/customErrors';
 
-const errorHandler = (err: Error, req: Request, res: Response) => {
+const errorHandler = (
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   if (err instanceof customError) {
     res.status(err.errorCode).send({ errors: err.serializeErrors() });
+    next();
   } else {
     res.send(err);
   }
--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -1,20 +1,21 @@
 import express from 'express';
 import adminController from '../controllers/admin';
-import { authenticate } from '../middlewares/authentication';
-import checkRole from '../middlewares/authorization';
+// import { authenticate } from '../middlewares/authentication';
+// import checkRole from '../middlewares/authorization';
+import { isAuthenticated, checkRole } from '../middlewares/isAuth';
 
 const router = express.Router();
 
 router.get(
   '/cooker',
-  authenticate,
+  isAuthenticated,
   checkRole('admin'),
   adminController.getCooker
 );
 
 router.get(
   '/customers',
-  authenticate,
+  isAuthenticated,
   checkRole('admin'),
   adminController.getCustomers
 );
